00f3ce7c60f8783dc70d891d9dace6d45c9ff0f1
Jibran F <jibran.fakhruddin@gmail.com>
2024-03-06 21:01:07 -0600

    connected mongoDB cluster to backend server

--- server/app.py
+++ server/app.py
@@ -1,8 +1,22 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+mongo_uri = os.getenv('MONGO_DB_URI')
+client = MongoClient(mongo_uri)
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 @app.route('/api/hello')
 def some_endpoint():
