88790b11e892f1988f46c42b9f8cca169799188a
Jeonghyeon Park <jpark634@wisc.edu>
2024-03-10 06:45:56 -0500

    Updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,91 +1,71 @@
-import { useEffect, useContext, useCallback } from "react";
-import Context, { QuickstartProvider } from "./Context";
-import Link from "./components/Link";
+import { useEffect, useState, useCallback } from "react";
+import { usePlaidLink } from "react-plaid-link";
 
 const App = () => {
-  const { linkSuccess, isItemAccess, isPaymentInitiation, dispatch } = useContext(Context);
+  const [linkToken, setToken] = useState(null);
 
-  const getInfo = useCallback(async () => {
-    const response = await fetch("/api/info", { method: "POST" });
-    if (!response.ok) {
-      dispatch({ type: "SET_STATE", state: { backend: false } });
-      return { paymentInitiation: false };
-    }
-    const data = await response.json();
-    const paymentInitiation = data.products.includes("payment_initiation");
-    dispatch({
-      type: "SET_STATE",
-      state: {
-        products: data.products,
-        isPaymentInitiation: paymentInitiation,
+  // Set access token
+  const OnSuccess = useCallback(async (publicToken) => {
+    await fetch("/api/set_access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
+      body: JSON.stringify({ public_token: publicToken }),
     });
-    return { paymentInitiation };
-  }, [dispatch]);
+  }, []);
 
-  const generateToken = useCallback(
-    async (isPaymentInitiation) => {
-      const path = isPaymentInitiation
-        ? "/api/create_link_token_for_payment"
-        : "/api/create_link_token";
-      const response = await fetch(path, {
-        method: "POST",
-      });
-      if (!response.ok) {
-        dispatch({ type: "SET_STATE", state: { linkToken: null } });
-        return;
-      }
+  // Creates a Link token
+  const createLinkToken = useCallback(async () => {
+    if (window.location.href.includes("?oauth_state_id=")) {
+      const token = localStorage.getItem('link_token'); // For OAuth, use previously generated Link token
+      setToken(token);
+    } else {
+      const response = await fetch("/api/create_link_token", {method: "POST"});
       const data = await response.json();
-      if (data && data.error != null) {
-        dispatch({
-          type: "SET_STATE",
-          state: {
-            linkToken: null,
-            linkTokenError: data.error,
-          },
-        });
-        return;
-      }
-      dispatch({ type: "SET_STATE", state: { linkToken: data.link_token } });
-      localStorage.setItem("link_token", data.link_token);
-    },
-    [dispatch]
-  );
+      setToken(data.link_token);
+      localStorage.setItem('link_token', data.link_token);
+    }
+  }, [setToken]);
+
+  let isOauth = false;
+
+  const config = {
+    linkToken,
+    OnSuccess,
+  };
+
+  console.log(window.location.href);
+  config.receivedRedirectUri = window.location.href;
+  isOauth = true;
+
+  // For OAuth, configure the received redirect URI
+  if (window.location.href.includes("?oauth_state_id=")) {
+    config.receivedRedirectUri = window.location.href;
+    isOauth = true;
+  }
+
+  const { open, ready } = usePlaidLink(config);
 
   useEffect(() => {
-    const init = async () => {
-      const { paymentInitiation } = await getInfo();
-      if (window.location.href.includes("?oauth_state_id=")) {
-        dispatch({
-          type: "SET_STATE",
-          state: {
-            linkToken: localStorage.getItem("link_token"),
-          },
-        });
-        return;
-      }
-      generateToken(paymentInitiation);
-    };
-    init();
-  }, [dispatch, generateToken, getInfo]);
+    if (linkToken == null) {
+      createLinkToken();
+    }
+    if (isOauth && ready) {
+      open();
+    }
+  }, [createLinkToken,linkToken, isOauth, ready, open]);
 
   return (
     <div>
-      <div>
-        {linkSuccess && isPaymentInitiation && isItemAccess && (
-          <Link />
-        )}
-      </div>
+      <button onClick={() => open()
+        } disabled={!ready}>
+        <strong>Link account</strong>
+      </button>
+      <h3>This is the generated token:</h3>
+      <h4>{linkToken}</h4>
     </div>
   );
 };
 
-// const AppWithProvider = () => {
-//   return (
-//     <QuickstartProvider>
-//       <App />
-//     </QuickstartProvider>
-//   );
-// };
-
-// export default AppWithProvider;
+export default App;
