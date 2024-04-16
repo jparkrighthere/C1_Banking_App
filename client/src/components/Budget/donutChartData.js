1616e0d6ef8f56cdb54de3970d291ff4dcbba5f4
Jibran F <jibran.fakhruddin@gmail.com>
2024-04-15 23:41:49 -0500

    added pie chart visualization widget, cleaned up budget page

---
+++ client/src/components/Budget/donutChartData.js
@@ -0,0 +1,11 @@
+
+// call backend route to get dynamic values for data for categorical spending
+
+export const data = [
+    {name:"Auto and Transportation", value: 96},
+    {name:"Bills and Utilities", value: 2029},
+    {name:"Health and Fitness", value: 342},
+    {name:"Shopping", value: 201},
+    {name:"Travel", value: 324},
+    {name:"Other", value: 67}
+]
